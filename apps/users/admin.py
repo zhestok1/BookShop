0090e019a3eb36cf673913a1e9f04d6a573f9e37
zhestok1 <rp3truhin@yandex.ru>
2026-08-02 17:57:51 +0400

    Создал модель юзера и зарегал его в админке

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительная информация', {'fields': ('avatar', 'phone', 'bio')}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Дополнительная информация', {'fields': ('avatar', 'phone', 'bio')}),
+    )
+    
+    list_display = ("username", "email", "phone", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    
+    phone = models.CharField(max_length=25, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+    
+    class Meta(AbstractUser.Meta):
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+    
+    def __str__(self):
+        return self.username
+        
+    
+    
